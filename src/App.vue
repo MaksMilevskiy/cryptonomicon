9edc663b1e52cfedcdb6f6b4a2ab71f362953203
Maks Milevski <max.milevski@gmail.com>
2022-04-25 21:41:00 +0300

    main

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+  <div class="container mx-auto flex flex-col items-center p-4">
     <div class="container">
       <div class="w-full my-4"></div>
       <section>
